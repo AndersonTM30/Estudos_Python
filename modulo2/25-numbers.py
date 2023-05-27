49e1a1f0b2ce911882cca66544055c0a57c55eef
AndersonTM30 <andersondev30@gmail.com>
2023-05-27 18:31:26 -0300

    Feat - Conceitos matemáticos e tipos de números em python

---
+++ modulo2/25-numbers.py
@@ -0,0 +1,19 @@
+a = 20
+b = 23.5
+print(type(a))
+print(type(b))
+
+#Operações matemáticas
+print(f'Soma a + b: {a + b}\nsubtração de a - b: {a - b}\nmultiplicação de a * b: {a * b}\ndivisao de a / b: {a / b}')
+print(a // b) #Divisão inteira
+print(a % b) #Resto da divisão
+print(a ** b) #Exponenciação
+
+#Operações de atribuição
+a += b #Soma a com b
+print(a)
+
+#Operações matemáticas comuns
+import math
+print(round(5.7))
+print(math.ceil(5.7))
