a0cb3c41e6416973c97fad11eb03eb7184f57334
AndersonTM30 <andersondev30@gmail.com>
2023-05-27 20:36:09 -0300

    Feat - Como debugar código python no VSCode

---
+++ modulo_2_tipos_de_dados/28-debugger.py
@@ -0,0 +1,7 @@
+
+def calcuar_preco_combo(pizza, refrigerante):
+        total = pizza + refrigerante
+        print(f'O preço total é {total}')
+
+calcuar_preco_combo(10, 5)
+print('programa finalizado')
