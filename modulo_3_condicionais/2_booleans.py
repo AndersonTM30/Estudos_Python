6d622963c04c33dc84eef7d18449a06cbe91504a
AndersonTM30 <andersondev30@gmail.com>
2023-05-28 00:22:51 -0300

    Feat - Booleanos com if, else e elif

---
+++ modulo_3_condicionais/2_booleans.py
@@ -0,0 +1,14 @@
+a = 30
+b = 20
+
+if a > b:
+    print('a é maior que b')
+else:
+    print('a é menor que b')
+
+if a == b:
+    print('a é igual a b')
+elif a != b:
+    print('a é diferente de b')
+else:
+    print('Número inválido!')
