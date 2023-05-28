1387005e6dc6a6a64b4f7b10f096f540240878a0
AndersonTM30 <andersondev30@gmail.com>
2023-05-28 10:25:30 -0300

    Feat - Adicionando itens no array

---
+++ modulo_3_condicionais/6_add_items_array.py
@@ -0,0 +1,14 @@
+list = []
+index = 1
+
+while index <= 10:
+    list.append(index)
+    index += 1
+    print(list)
+
+
+thisList = ['apple', 'banana', 'cherry', 'kiwi', 'mango']
+print(thisList)
+
+thisList.insert(4, 'strawberry')
+print(thisList)
