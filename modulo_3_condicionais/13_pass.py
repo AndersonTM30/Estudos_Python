e740c06339adfe217ea86f86b59582f10e3060d7
AndersonTM30 <andersondev30@gmail.com>
2023-05-28 11:32:24 -0300

    Feat - Conceito de pass no python

---
+++ modulo_3_condicionais/13_pass.py
@@ -0,0 +1,6 @@
+# Em resumo, a instrução pass é utilizada para indicar que não há nenhuma ação a ser executada em um determinado ponto do código. Ela é útil para manter a estrutura de um programa, sem executar nenhuma ação desnecessária.
+
+numero = 0
+while numero < 10:
+    pass
+print(numero)
