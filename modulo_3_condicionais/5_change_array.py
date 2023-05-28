2144dffc288513d5f6ad56eaffb456881dfe31d5
AndersonTM30 <andersondev30@gmail.com>
2023-05-28 00:50:48 -0300

    Feat - Alterando listas com pelo índice e com a função insert

---
+++ modulo_3_condicionais/5_change_array.py
@@ -0,0 +1,11 @@
+thisList = ['apple', 'banana', 'cherry', 'kiwi', 'mango']
+
+thisList[1] = 'orange'
+print(thisList)
+
+thisList[1:3] = ['berry', 'banana']
+
+print(thisList)
+
+thisList.insert(4, 'strawberry')
+print(thisList)
