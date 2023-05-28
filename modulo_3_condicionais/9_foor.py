c6712254fe6c311ffa6f040e525fe7119dd44845
AndersonTM30 <andersondev30@gmail.com>
2023-05-28 10:50:05 -0300

    Feat - Loop for em python

---
+++ modulo_3_condicionais/9_foor.py
@@ -0,0 +1,19 @@
+list = []
+for numero in range(1, 11, 2):
+    list.append(numero)
+    print(list)
+
+thisList = ['apple', 'banana', 'cherry', 'kiwi', 'mango']
+
+for nome in thisList:
+    print(nome)
+
+# Desafio 1
+# Usando um loop, exiba na tela: Estamos em X onde x é um valor iniciiando em 18 e finalizando em 110.
+for contador in range(18, 111):
+    print(f'Estamos em {contador}')
+
+# Desafio 2
+# Você precisa de 10 passos para finalizar uma tarefa, exia na tela , usando loop for a seguinte frase: Realisando o passo X
+for contador in range(1, 11):
+    print(f'Realisando o passo {contador}')
