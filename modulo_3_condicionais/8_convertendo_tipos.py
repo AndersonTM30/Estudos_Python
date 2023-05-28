b79d9898e628d1c9abb1e360c6310cf591fc37b6
AndersonTM30 <andersondev30@gmail.com>
2023-05-28 10:39:59 -0300

    Feat - Convertendo tipos em python

---
+++ modulo_3_condicionais/8_convertendo_tipos.py
@@ -0,0 +1,17 @@
+# Conversão de tipos
+
+idade = input('Qual sua idade? ')
+
+print(int(idade) > 18)
+
+print (type(str(5)))
+
+altura = input('Qual é a altura da parede? ')
+
+print(float(altura))
+
+texto = 'Python, Java, C++'
+
+print(list(texto))
+
+print(tuple(texto))
