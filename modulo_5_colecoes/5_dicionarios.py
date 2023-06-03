c4c3911d0830ab94759115097cfe53eccd41ae3f
AndersonTM30 <andersondev30@gmail.com>
2023-06-03 11:17:00 -0300

    Feat - Exercitando dicionários

--- modulo_5_colecoes/5_dicionarios.py
+++ modulo_5_colecoes/5_dicionarios.py
@@ -18,3 +18,56 @@ print(dicionario_pessoa.items())
 # Iterar sobre um dicionário
 for item in dicionario_pessoa.items():
     print(item[1])
+
+"""
+Adicionar aluno: O programa deve solicitar o nome do aluno, sua idade e seu número de matrícula. Em seguida,
+adicione essas informações a um dicionário, onde a matrícula será a chave e os dados do aluno serão o valor.
+"""
+lista_alunos = {}
+
+
+def cadastra_aluno():
+    aluno = input('Informe o nome do Aluno: ')
+    matricula = input('Informe o número da matrícula do aluno: ')
+    lista_alunos[matricula] = {'nome': aluno, 'matricula': matricula}
+
+"""
+Consultar aluno: O programa deve solicitar o número de matrícula de um aluno e exibir suas informações (nome e idade) caso ele exista no
+sistema. Caso contrário, exiba uma mensagem informando que o aluno não foi encontrado.
+"""
+
+def consultar_aluno():
+    print('Consultando aluno:')
+    matricula = input('Informe a matrícula do aluno: ')
+    if matricula in lista_alunos:
+        aluno = lista_alunos[matricula]
+        print('Informações do aluno: {aluno}')
+        print(f'Nome: {aluno["nome"]}\nMatricula: {aluno["matricula"]}')
+    else:
+        print('Aluno não encontrado!')
+
+"""
+Remover aluno: O programa deve solicitar o número de matrícula de um aluno e removê-lo do sistema, caso ele exista. Caso contrário, exiba uma
+mensagem informando que o aluno não foi encontrado.
+"""
+
+def remover_aluno():
+    matricula = input('Informe a matrícula do aluno: ')
+    if matricula in lista_alunos:
+        del lista_alunos[matricula]
+        print('Aluno removido com sucesso!')
+    else:
+        print('Aluno não encontrado!')
+
+
+while True:
+    cadastra_aluno()
+    decisao = input('Deseja continuar cadastrando alunos? ')
+    if decisao.lower() not in ('sim', 's'):
+        break
+
+consultar_aluno()
+
+remover_aluno()
+
+print(lista_alunos)
