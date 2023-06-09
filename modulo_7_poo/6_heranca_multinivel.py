39fef3d848c5a315791b428b7fba22b81b268c6c
AndersonTM30 <andersondev30@gmail.com>
2023-06-09 07:22:06 -0300

    Feat - Conceito de herança multinível

---
+++ modulo_7_poo/6_heranca_multinivel.py
@@ -0,0 +1,12 @@
+class Veiculo:
+    pass
+class VeiculoDeRodas(Veiculo):
+    pass
+class Carro(VeiculoDeRodas):
+    pass
+class CarroEletrico(Carro):
+    pass
+class CarroEletricoDuasPortas(CarroEletrico):
+    pass
+
+# Esse é um tipo de abordagem que pode gerar um código espaguete, ajusta em um local e quebra em outro.
