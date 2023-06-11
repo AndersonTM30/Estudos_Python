54999546b6262f26caf8951ee62b1cc1909bfb60
AndersonTM30 <andersondev30@gmail.com>
2023-06-11 14:36:12 -0300

    Feat - Criando planilhas excel com python

---
+++ modulo_9_manipulando_arquivos/6_criando_planilhas/app.py
@@ -0,0 +1,31 @@
+"""
+Sheets - São as páginas de uma planilha
+Workboot - É o arquivo que contém estas páginas
+"""
+
+import openpyxl as excel
+# Criar planilha
+planilha_teste = excel.Workbook()
+planilha_teste.create_sheet('Frutas')
+planilha_teste.create_sheet('Legumes')
+planilha_teste.create_sheet('Sementes')
+print(planilha_teste.sheetnames)
+
+# Cria o cabeçalho da planilha
+cabecalho_frutas = ['Título', 'Localização', 'Preço']
+sheet_frutas = planilha_teste.get_sheet_by_name('Frutas')
+sheet_frutas.append(cabecalho_frutas)
+
+# Salvando dados na planilha
+frutas = [
+    ['Uva', 'Mercado', 5],
+    ['Melancia', 'Mercado', 15],
+    ['Bolo', 'Mercado', 40]
+]
+
+for fruta in frutas:
+    sheet_frutas.append(fruta)
+
+    
+# Salva a planilha criada
+planilha_teste.save('Dados Supermercado.xlsx')
