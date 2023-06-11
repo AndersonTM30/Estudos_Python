16a3d484f1bf1e64dd5310146acb654cb44b19b9
AndersonTM30 <andersondev30@gmail.com>
2023-06-10 21:31:27 -0300

    Feat - Refatorando o código em python

---
+++ modulo_8_modulos/8_refatoracao/app.py
@@ -0,0 +1,23 @@
+
+class Calculadora:
+    pass
+
+
+calc1 = Calculadora()
+calc2 = Calculadora()
+calc3 = Calculadora()
+
+print(calc1)
+
+"""
+Para renomear em massa, seleciona o nome que deseja, tecla F2, coloca o novo nome, tecla shift + enter para
+escolher se vai mudar todos ou cancelar a alteração
+"""
+def adicao():
+    resultado = 20 + 50
+
+adicao()
+
+altura = 60
+largura = 2
+tamanho = (altura / largura) + 50
