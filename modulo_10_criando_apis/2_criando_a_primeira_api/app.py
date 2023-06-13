68777705a7d0744c63da7fd2d74baffc6708eef2
AndersonTM30 <andersondev30@gmail.com>
2023-06-13 07:36:34 -0300

    Feat - Criando uma rota de consulta por id

--- modulo_10_criando_apis/2_criando_a_primeira_api/app.py
+++ modulo_10_criando_apis/2_criando_a_primeira_api/app.py
@@ -8,5 +8,11 @@ app = Flask(__name__)
 def obter_postagens():
     return jsonify(postagens)
 
+# consulta por id
+# Exemplo: http://localhost:5000/postagens/1
+@app.route('/postagens/<int:indice>', methods=['GET'])
+def obter_postagem_por_id(indice):
+    return jsonify(postagens[indice], 200)
+
 # Executando o servidor
 app.run(port=5000, host='localhost', debug=True)
